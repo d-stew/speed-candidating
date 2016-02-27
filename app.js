615d76fa1934bbd818deba2eea18b01359026533
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-27 02:47:41 -0700

    Start work on issues page

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
   $('div').hide();
+  $('div .issues').hide();
 
-  $('.enter').on('click', function(){
+  $('button').mouseenter(function() {
+    $(this).filter(':not(:animated)').animate({height: '+=15px', width: '+=15px'});
+  }, function() {
+    $(this).animate({height: '+=15px', width: '+=15px'});
+      });
+  $('button').mouseleave(function() {
+    $(this).filter(':not(:animated)').animate({height: '-=15px', width: '-=15px'});
+  }, function() {
+    $(this).animate({height: '-=15px', width: '-=15px'});
+      });
+
+  $('.enter-button').on('click', function(){
     $('.landing').slideUp();
     $('div').show();
-  })
-})
+  });
+
+  $('.start-button').on('click', function(){
+    $('.about-main').slideUp(1000);
+    $('.top-bar').animate({'margin':'1.5% 0 0 0'}, 1000);
+    $('.img-circle').animate({'height':'100px','width':'100px'}, 1000);
+  });
+
+});
