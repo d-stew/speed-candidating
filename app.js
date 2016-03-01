52b814ba33e1eadb491b2232d36aed101f79995c
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-03-01 10:37:09 -0700

    Store user respones in a JS object

--- app.js
+++ app.js
@@ -26,93 +26,115 @@ $(document).ready(function(){
     $('.img-circle').animate({'height':'100px','width':'100px'}, 1000);
   });
 
+  var user = {};
+
   $('.gun-button').click(function(){
+    user.guns = this.value;
     $('.guns').slideUp();
     $('.crime').fadeIn();
   });
 
   $('.crime-button').click(function() {
+    user.crime = this.value;
     $('.crime').slideUp();
     $('.healthcare').fadeIn();
   });
 
   $('.healthcare-button').click(function(){
+    user.healthcare = this.value;
     $('.healthcare').slideUp();
     $('.energy').fadeIn();
   });
 
   $('.energy-button').click(function(){
+    user.energy = this.value;
     $('.energy').slideUp();
-    $('.pot').fadeIn();
+    $('.drugs').fadeIn();
   });
 
-  $('.pot-button').click(function(){
-    $('.pot').slideUp();
+  $('.drugs-button').click(function(){
+    user.drugs = this.value;
+    $('.drugs').slideUp();
     $('.taxes').fadeIn();
   });
 
   $('.taxes-button').click(function(){
+    user.taxes = this.value;
     $('.taxes').slideUp();
     $('.immigration').fadeIn();
   });
 
   $('.immigration-button').click(function(){
+    user.immigration = this.value;
     $('.immigration').slideUp();
     $('.ss').fadeIn();
   });
 
   $('.ss-button').click(function(){
+    user.ss = this.value;
     $('.ss').slideUp();
     $('.military').fadeIn();
   });
 
   $('.military-button').click(function(){
+    user.military = this.value;
     $('.military').slideUp();
     $('.fp').fadeIn();
   });
 
   $('.fp-button').click(function(){
+    user.fp = this.value;
     $('.fp').slideUp();
     $('.tpp').fadeIn();
   });
 
   $('.tpp-button').click(function(){
+    user.tpp = this.value;
     $('.tpp').slideUp();
     $('.marriage').fadeIn();
   });
 
   $('.marriage-button').click(function(){
+    user.marriage = this.value;
     $('.marriage').slideUp();
     $('.religion').fadeIn();
   });
 
   $('.religion-button').click(function(){
+    user.religion = this.value;
     $('.religion').slideUp();
     $('.epa').fadeIn();
   });
 
   $('.epa-button').click(function(){
+    user.epa = this.value;
     $('.epa').slideUp();
     $('.voting').fadeIn();
   });
 
   $('.voting-button').click(function(){
+    user.voting = this.value;
     $('.voting').slideUp();
     $('.nsa').fadeIn();
   });
 
   $('.nsa-button').click(function(){
+    user.nsa = this.value;
     $('.nsa').slideUp();
     $('.wages').fadeIn();
   });
 
   $('.wages-button').click(function(){
+    user.wages = this.value;
     $('.wages').slideUp();
     $('.citizens').fadeIn();
   });
 
   $('.citizens-button').click(function(){
+    user.citizens = this.value;
     $('.citizens').slideUp();
     $('.match').fadeIn('slow');
+    console.log(user);
   });
+
 });
